6899b0fd84ce30cef17fe9276d2aadabd9a5a1e2
BoPras <106642078+BoPras@users.noreply.github.com>
2023-02-18 22:26:46 +0700

    Add files via upload

---
+++ SQLQuery3.sql
@@ -0,0 +1,6 @@
+---BoPras---
+use Northwind;
+
+SELECT CONCAT(emp.TitleOfCourtesy, ' ', emp.FirstName, ' ', emp.LastName) AS [Full Name], emp.HomePhone AS [Phone]
+FROM dbo.Employees AS emp
+WHERE emp.Country = 'USA';
